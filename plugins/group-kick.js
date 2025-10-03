07b5c23e8b25f8b63afa67f33b4fcb17414e5540
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-10-03 01:21:11 -0300

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,18 +1,23 @@
 var handler = async (m, { conn, participants, usedPrefix, command }) => {
-let mentionedJid = await m.mentionedJid
-let user = mentionedJid && mentionedJid.length ? mentionedJid[0] : m.quoted && await m.quoted.sender ? await m.quoted.sender : null
-if (!user) return conn.reply(m.chat, `❀ Debes mencionar a un usuario para poder expulsarlo del grupo.`, m)
-try {
-const groupInfo = await conn.groupMetadata(m.chat)
-const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net'
-const ownerBot = global.owner[0][0] + '@s.whatsapp.net'
-if (user === conn.user.jid) return conn.reply(m.chat, `ꕥ No puedo eliminar el bot del grupo.`, m)
-if (user === ownerGroup) return conn.reply(m.chat, `ꕥ No puedo eliminar al propietario del grupo.`, m)
-if (user === ownerBot) return conn.reply(m.chat, `ꕥ No puedo eliminar al propietario del bot.`, m)
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-} catch (e) {
-conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${e.message}`, m)
-}}
+    let mentionedJid = await m.mentionedJid
+    let user = mentionedJid && mentionedJid.length ? mentionedJid[0] : m.quoted && await m.quoted.sender ? await m.quoted.sender : null
+    if (!user) return conn.reply(m.chat, `⚠️ ❀ Debes mencionar a un intruso que quieras expulsar del grupo… Los animatrónicos no permitirán que quede libre.`, m)
+
+    try {
+        const groupInfo = await conn.groupMetadata(m.chat)
+        const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net'
+        const ownerBot = global.owner[0][0] + '@s.whatsapp.net'
+
+        if (user === conn.user.jid) return conn.reply(m.chat, `🛑 No puedo eliminarme a mí mismo… los animatrónicos tampoco me dejarían.`, m)
+        if (user === ownerGroup) return conn.reply(m.chat, `⚠️ No puedo eliminar al creador del grupo… incluso los animatrónicos lo respetan.`, m)
+        if (user === ownerBot) return conn.reply(m.chat, `⚠️ No puedo eliminar al propietario del bot. Su presencia es inquebrantable.`, m)
+
+        await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+        conn.reply(m.chat, `🔪 El intruso ${user.split('@')[0]} ha sido expulsado… los animatrónicos vigilan que no regrese.`, m)
+    } catch (e) {
+        conn.reply(m.chat, `⚠︎ Ha ocurrido un error en la operación.\n> Usa *${usedPrefix}report* para reportarlo.\n\n${e.message}`, m)
+    }
+}
 
 handler.help = ['kick']
 handler.tags = ['grupo']
