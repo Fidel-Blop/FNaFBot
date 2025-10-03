28d5d2ccd201f177d88ef4ea97ef34466f6f05b9
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-10-03 01:22:46 -0300

    Update group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -1,17 +1,22 @@
 var handler = async (m, { conn, usedPrefix, command, text, groupMetadata, isAdmin }) => {
-let mentionedJid = await m.mentionedJid
-let user = mentionedJid && mentionedJid.length ? mentionedJid[0] : m.quoted && await m.quoted.sender ? await m.quoted.sender : null
-if (!user) return conn.reply(m.chat, `❀ Debes mencionar a un usuario para poder promoverlo a administrador.`, m)
-try {
-const groupInfo = await conn.groupMetadata(m.chat)
-const ownerGroup = groupInfo.owner || m.chat.split('-')[0] + '@s.whatsapp.net'
-if (user === ownerGroup || groupInfo.participants.some(p => p.id === user && p.admin))
-return conn.reply(m.chat, 'ꕥ El usuario mencionado ya tiene privilegios de administrador.', m)
-await conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-await conn.reply(m.chat, `❀ Fue agregado como admin del grupo con exito.`, m)
-} catch (e) {
-conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${e.message}`, m)
-}}
+    let mentionedJid = await m.mentionedJid
+    let user = mentionedJid && mentionedJid.length ? mentionedJid[0] : m.quoted && await m.quoted.sender ? await m.quoted.sender : null
+    if (!user) return conn.reply(m.chat, `🎭 ¡Atención! Debes mencionar a un usuario para que reciba privilegios en esta sala...`, m)
+    
+    try {
+        const groupInfo = await conn.groupMetadata(m.chat)
+        const ownerGroup = groupInfo.owner || m.chat.split('-')[0] + '@s.whatsapp.net'
+        
+        if (user === ownerGroup || groupInfo.participants.some(p => p.id === user && p.admin))
+            return conn.reply(m.chat, '⚠️ El animatrónico mencionado ya tiene poderes de supervisor.', m)
+        
+        await conn.groupParticipantsUpdate(m.chat, [user], 'promote')
+        
+        await conn.reply(m.chat, `🎵 ¡Atención! ${user.split('@')[0]} ha sido promovido a administrador del grupo.\n🕹️ Ahora vigilará la sala con nosotros.`, m)
+    } catch (e) {
+        conn.reply(m.chat, `⚠︎ Se ha producido un fallo en el sistema de vigilancia.\n> Usa *${usedPrefix}report* para reportarlo.\n\n${e.message}`, m)
+    }
+}
 
 handler.help = ['promote']
 handler.tags = ['grupo']
