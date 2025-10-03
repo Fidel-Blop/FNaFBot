997ce3ea6abf02584239acadd0c18fce1aca9573
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-10-03 02:19:28 -0300

    Update rpg-balance.js

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -1,23 +1,23 @@
 let handler = async (m, { conn, usedPrefix }) => {
 if (!db.data.chats[m.chat].economy && m.isGroup) {
-return m.reply(`《✦》Los comandos de *Economía* están desactivados en este grupo.\n\nUn *administrador* puede activarlos con el comando:\n» *${usedPrefix}economy on*`)
+return m.reply(`👻 《✦》Los comandos de *Economía* están desactivados en este grupo.\n\nUn *administrador* puede activarlos con el comando:\n» *${usedPrefix}economy on*`)
 }
 let mentionedJid = await m.mentionedJid
 let who = mentionedJid[0] ? mentionedJid[0] : m.quoted ? await m.quoted.sender : m.sender
 let name = await (async () => global.db.data.users[who].name || (async () => { try { const n = await conn.getName(who); return typeof n === 'string' && n.trim() ? n : who.split('@')[0] } catch { return who.split('@')[0] } })())()
-if (!(who in global.db.data.users)) return m.reply(`ꕥ El usuario no se encuentra en mi base de datos.`)
+if (!(who in global.db.data.users)) return m.reply(`⚠️ El usuario no se encuentra en mi base de datos.`)
 let user = global.db.data.users[who]
 let coin = user.coin || 0
 let bank = user.bank || 0
 let total = (user.coin || 0) + (user.bank || 0)
-const texto = `ᥫ᭡ Informacion -  Balance ❀
- 
-ᰔᩚ Usuario » *${name}*   
-⛀ Cartera » *¥${coin.toLocaleString()} ${currency}*
-⚿ Banco » *¥${bank.toLocaleString()} ${currency}*
-⛁ Total » *¥${total.toLocaleString()} ${currency}*
 
-> *Para proteger tu dinero, ¡depósitalo en el banco usando #deposit!*`
+const texto = `🕹️ *Balance de ${name}* 🕹️
+
+👁️ Cartera » *¥${coin.toLocaleString()} ${currency}*  
+🏦 Banco » *¥${bank.toLocaleString()} ${currency}*  
+💰 Total » *¥${total.toLocaleString()} ${currency}*
+
+⚠️ ¡Cuidado con los animatronicos! Deposita tu dinero en el banco usando *#deposit* para mantenerlo seguro durante la noche.`
 await conn.reply(m.chat, texto, m)
 }
 
