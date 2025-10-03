6a5ad8838c1c3d772ec70196dc7ac27172947992
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-10-03 01:21:58 -0300

    Update group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -1,9 +1,15 @@
 var handler = async (m, { conn, args }) => {
-let group = m.chat
-const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-let message = `\t*⌁☍꒷₊˚ Link del Grupo ꒷₊˚⌁☍*\n\n> \`Link:\` ${link}`
-await conn.sendMessage(group, { image: { url: pp }, caption: message })
+    let group = m.chat
+    const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
+    let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+    let message = `
+🎵 𝗔𝗟𝗘𝗥𝗧𝗔 𝗗𝗘 𝗜𝗡𝗩𝗜𝗧𝗔𝗖𝗜Ó𝗡 🎵
+
+👁️‍🗨️ Enlace de acceso a la sala:
+> \`Link:\` ${link}
+
+⚠️ Recuerda: las cámaras están activas. Los animatrónicos vigilan cada llegada...`
+    await conn.sendMessage(group, { image: { url: pp }, caption: message.trim() })
 }
 
 handler.help = ['link']
