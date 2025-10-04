eaf749654756f68dbcdd0d991f2440b392e3fa40
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-10-03 23:45:14 -0300

    Update tools-fetch.js

--- plugins/tools-fetch.js
+++ plugins/tools-fetch.js
@@ -3,22 +3,28 @@ import { format } from 'util'
 
 let handler = async (m, { conn, usedPrefix, text }) => {
 if (m.fromMe) return
-if (!/^https?:\/\//.test(text)) return m.reply(`❀ Por favor, ingresa la *url* de la pagina.`)
+if (!/^https?:\/\//.test(text)) 
+  return m.reply(`🎭 *[ACCESO DENEGADO]* 🎭\n\nIngresa la *URL* que deseas inspeccionar en el sistema de cámaras.`)
+
 let url = text
-await m.react('🕒')
+await m.react('🕒') // animación de carga como si fuera la pantalla de seguridad
+
 let res = await fetch(url)
 if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
-throw `Content-Length: ${res.headers.get('content-length')}`
+throw `⚠️ *ARCHIVO DEMASIADO PESADO* ⚠️\n\nContent-Length: ${res.headers.get('content-length')}`
 }
-if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
+
+if (!/text|json/.test(res.headers.get('content-type'))) 
+  return conn.sendFile(m.chat, url, 'file', `📂 *Archivo detectado en el sistema.*\n\n🗂️ Origen: ${text}`, m)
+
 let txt = await res.buffer()
 try {
-txt = format(JSON.parse(txt + ''))
+  txt = format(JSON.parse(txt + ''))
 } catch (e) {
-txt = txt + ''
+  txt = txt + ''
 } finally {
-m.reply(txt.slice(0, 65536) + '')
-await m.react('✔️')
+  m.reply(`📟 *[ACCESO A LOS ARCHIVOS DEL SISTEMA]* 📟\n\n${txt.slice(0, 65536)}`)
+  await m.react('✔️') // ✅ como confirmación de que el "sistema" funcionó
 }}
 
 handler.help = ['get']
