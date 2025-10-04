b3b130bb7bed7736143038608b945435b9a7fa24
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-10-03 23:55:48 -0300

    Update tools-toimg.js

--- plugins/tools-toimg.js
+++ plugins/tools-toimg.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!m.quoted) {
-return conn.reply(m.chat, `❀ Debes citar un sticker para convertir a imagen.`, m)
-}
-await m.react('🕒')
-let xx = m.quoted
-let imgBuffer = await xx.download()   
-if (!imgBuffer) {
-await m.react('✖️')
-return conn.reply(m.chat, `ꕥ No se pudo descargar el sticker.`, m)
-}
-await conn.sendMessage(m.chat, { image: imgBuffer, caption: '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*' }, { quoted: m })
-await m.react('✔️')
+  if (!m.quoted) {
+    return conn.reply(m.chat, `❀ Music Man dice: Debes citar un sticker para convertirlo a imagen.`, m)
+  }
+  await m.react('🕒')
+  let xx = m.quoted
+  let imgBuffer = await xx.download()   
+  if (!imgBuffer) {
+    await m.react('✖️')
+    return conn.reply(m.chat, `ꕥ Music Man dice: No se pudo descargar el sticker.`, m)
+  }
+  await conn.sendMessage(m.chat, { image: imgBuffer, caption: '❀ Music Man dice: ¡Aquí tienes tu imagen! 🔴' }, { quoted: m })
+  await m.react('✔️')
 }
 
 handler.help = ['toimg']
